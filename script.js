9c6be613c3ef8329a516bdebb64242d63d93c6f9
skmyj <131327419+skmyj@users.noreply.github.com>
2025-10-07 16:11:15 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -7,6 +7,20 @@ $(document).ready(function(){
     });
   });
  
+  document.getElementById('js-download-btn').addEventListener('click', function() {
+    var fileUrl = 'ファイルへのパス'; // ダウンロードするファイルのURL
+    var fileName = 'sample.pdf'; // ダウンロードする際のファイル名
+
+    var a = document.createElement('a');
+    a.style.display = 'none';
+    a.href = fileUrl;
+    a.download = fileName;
+
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+});
+
 <button onclick="history.back()">戻る</button>
 
   if (history.length > 1) {
